d6769ce13f654ac7664a543bd1b9a24b63832ce0
david hays <purplehays527@gmail.com>
2020-11-14 10:40:12 -0700

    it is looking good so far, I have done the migrations set up for knex and the packagae.json. Additionally, I set up the server.json shell and the index.json file

---
+++ api/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const helmet = require("helmet");
+const morgan = require('morgan')
+
+// MAKE THE AUTH ROUTER 
+// const authRouter = require("../auth/auth-router.js");
+// const usersRouter = require("../users/users-router.js");
+
+const server = express();
+
+server.use(helmet());
+server.use(morgan('dev'))
+server.use(express.json());
+server.use(cors());
+
+// server.use("/api/auth", authRouter);
+// server.use("/api/users", usersRouter);
+
+server.get("/", (req, res) => {
+  res.json({ api: "up" });
+});
+
+module.exports = server;
